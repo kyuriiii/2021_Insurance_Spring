936092fb89169c6ff4a7913ced8db3eb01661bd2
kyuriiii <47288015+kyuriiii@users.noreply.github.com>
2021-12-05 12:05:29 +0900

    dao 삭제

--- src/main/java/com/insurance/Insurance_spring/domain/dao/EmployeeDao.java
+++
@@ -1,8 +0,0 @@
-package com.insurance.Insurance_spring.domain.dao;
-
-
-import com.insurance.Insurance_spring.domain.employee.EmployeeList;
-
-public interface EmployeeDao {
-	public EmployeeList retrieve();
-}
